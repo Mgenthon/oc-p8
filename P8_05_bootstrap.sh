c866ba1da2d44868abc4b72b1648a6701f4bb21a
Mathieu GENTHON <67680088+Mgenthon@users.noreply.github.com>
2020-11-15 13:54:56 +0100

    portfolio creation

---
+++ P8_05_bootstrap.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+sudo python3 -m pip install --upgrade setuptools
+sudo python3 -m pip install --upgrade pip
+sudo python3 -m pip install wheel
+sudo python3 -m pip install pillow
+sudo python3 -m pip install pandas
+sudo python3 -m pip install tensorflow==1.14
+sudo python3 -m pip install pyarrow
+sudo python3 -m pip install boto3
